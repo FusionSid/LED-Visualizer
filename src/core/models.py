2c50c1ef1139b62f6e64ee565c2fc53384cf0976
FusionSid <siddheshadsv@icloud.com>
2025-07-17 19:14:17 +1200

    fix: properly close the midi input on exit

--- src/core/models.py
+++ src/core/models.py
@@ -8,6 +8,7 @@ from core.lib import clear_all_leds
 class MidiContextManager:
     def __init__(self, strip):
         self.strip = strip
+        self.midi_input = None
 
     def __enter__(self):
         print("Waiting for MIDI input device...")
@@ -17,11 +18,14 @@ class MidiContextManager:
                 break
             time.sleep(1)
 
-        device_name = inputs[0]
-        with mido.open_input(device_name) as midi_input:  # type: ignore
-            print(f"Connected to MIDI device: {device_name}")
-            return midi_input
+        device_name = inputs[1]  # inputs[1] is my piano
+        self.midi_input = mido.open_input(device_name)  # type: ignore
+        print(f"Connected to MIDI device: {device_name}")
+
+        return self.midi_input
 
     def __exit__(self, *_):
         print("Exiting. Turning off LEDs...")
         clear_all_leds(self.strip)
+        if self.midi_input is not None:
+            self.midi_input.close()
